31bbbb034939ec98e996e582935cd36dfb9291d2
loyolastalin <loyolastalin@gmail.com>
2020-07-02 23:09:18 +0530

    -  updatede

---
+++ collection/errorprg.py
@@ -0,0 +1,17 @@
+def searchNumber(kuppans, number):
+    for item in items:
+        if(item == number):
+            return True
+    return False
+
+def funcname(ss1):
+    for target_list in ss1:
+        pass
+
+def main():
+    numbers = range(1,101)
+    searchNumber(numbers, 1)
+    
+    ss =[1,2,2]
+    funcname(ss)
+   
---
+++ collection/experiement.py
@@ -0,0 +1,13 @@
+def funcname(ss):
+    print(type(ss))
+
+def funcname1(ss):
+    print(type(ss))
+
+def main():
+    a = 10
+    funcname(a)
+    funcname1(20)
+
+
+main()
